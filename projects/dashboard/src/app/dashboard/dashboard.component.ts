6cfca206e1982cf28bb6bffec216b09d40aefab4
Faizal Vasaya <faizal.vasaya@1rivet.com>
2019-06-12 16:05:01 +0530

    feat: Proxying assets folder for each micro-app

--- projects/dashboard/src/app/dashboard/dashboard.component.ts
+++ projects/dashboard/src/app/dashboard/dashboard.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from "@angular/core";
 
 @Component({
-  selector: 'app-dashboard',
-  templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  selector: "app-dashboard",
+  templateUrl: "./dashboard.component.html",
+  styleUrls: ["./dashboard.component.css"],
+  encapsulation: ViewEncapsulation.Emulated
 })
 export class DashboardComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
